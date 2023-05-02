60a55f94ad26567beb12cbcdd5d4e069a5a4ee98
Brendan <bziegenhorn6503@stu.d214.org>
2023-05-02 09:47:07 -0500

    Timer

---
+++ CardioTrackerView.swift
@@ -0,0 +1,51 @@
+
+
+import SwiftUI
+
+var longestRun = " "
+var lastRun = " "
+
+struct CardioTrackerView: View {
+    
+    @State var timeRemaining = 62
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    func convertSecondsToTime(timeInSeconds : Int) -> String {
+        
+        let minutes = timeInSeconds / 60
+        
+        let seconds = timeInSeconds % 60
+        
+        return String(format: "%02i:%02i", minutes, seconds)
+    }
+    
+    var body: some View {
+        VStack{
+            Text("Cardio Tracker")
+                .font(.title)
+            Text(convertSecondsToTime(timeInSeconds:timeRemaining))
+                .padding()
+                .font(.system(size: 100))
+                .onReceive(timer) { _ in
+                    
+                    timeRemaining -= 1
+                    
+                }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            CardioTrackerView()
+        }
+        
+        
+        
+        struct CardioTrackerView_Previews: PreviewProvider {
+            static var previews: some View {
+                CardioTrackerView()
+            }
+        }
+    }
+}
